971f8db94c428c14fc60b069483c6bb75e8a83b2
scammi <scammi@gmail.com>
2021-03-31 16:03:21 -0300

    Push

---
+++ app/src/main/java/com/example/nav_comp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.nav_comp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var navigationView = findViewById(R.id.navigationView) as NavigationView
+
+        navController = findNavController(R.id.fragment)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        navigationView.setupWithNavController(navController)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+Source code Navigation Drawer with Navigation Component - Android Studio Tutorial by Stevdza-San
+
